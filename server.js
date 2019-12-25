9bed64db2f3c94f8cccdb278eb7002445f3617ae
juholehtimaki <juho.a.lehtimaki@gmail.com>
2019-12-25 17:31:11 +0200

    backend with config and updated .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const sendGrid = require("@sendGrid/mail");
+const config = require("./config.json"); //includes target email and sendgrid_api_key
 
 const app = express();
 
@@ -7,6 +9,18 @@ app.use(cors());
 
 app.post("/contact", (req, res) => {
   console.log("attempting to send an email");
+  console.log(req.body);
+  sendGrid.setApiKey(config.SENDGRID_API_KEY);
+  const msg = {
+    to: config.MY_EMAIL,
+    from: req.body.email,
+    subject: "Portfolio - contact me",
+    text: req.body.msg
+  };
+  sendGrid
+    .send(msg)
+    .then(result => res.status(200).send())
+    .catch(err => res.status(400).send());
 });
 
 const port = process.env.PORT || 5000;
